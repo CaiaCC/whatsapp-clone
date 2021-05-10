3761770fde4a364d45a2f0e2c6e7dcc688934b03
Caia Chuang <caiachuang@gmail.com>
2021-05-10 16:06:00 -0700

    Connect with pusher.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
+import { useEffect } from "react";
+import Pusher from "pusher-js";
 import "./App.css";
 import Sidebar from "./components/Sidebar";
 import Chat from "./components/Chat";
 
-
 const App = () => {
+    useEffect(() => {
+        const pusher = new Pusher("163612a7213d95a362bb", {
+            cluster: "mt1",
+        });
+
+        const channel = pusher.subscribe("messages");
+        channel.bind("inserted", function (data) {
+            alert(JSON.stringify(data));
+        });
+    }, []);
+
     return (
         <div className="app">
             <div className="app__body">
