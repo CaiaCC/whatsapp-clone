432359f80687dcb6e755cf984b77e3be374c6b07
Caia Chuang <caiachuang@gmail.com>
2021-05-09 21:18:00 -0700

    Style App and index.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-import './App.css';
+import "./App.css";
 
 const App = () => {
-  return (
-    <div className="app">
-
-    </div>
-  );
-}
+    return (
+        <div className="app">
+            <div className="app__body"></div>
+        </div>
+    );
+};
 
 export default App;
