ce3a1cb03b974d2294c60eb815e74508bca188f7
Caia Chuang <caiachuang@gmail.com>
2021-05-09 23:30:42 -0700

    Add search in the Sidebar.

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -21,6 +21,14 @@ const Sidebar = () => {
                     </IconButton>
                 </div>
             </div>
+
+            <div className="sidebar__search">
+                <div className="sidebar__searchContainer">
+                    <SearchOutlined />
+                    <input placeholder="Search or start new chat" type="text" />
+                </div>
+            </div>
+
         </div>
     );
 };
