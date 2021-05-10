742dcd4bb03ddca784d228f03019667ee24d71d3
Caia Chuang <caiachuang@gmail.com>
2021-05-09 21:39:05 -0700

    Add Avatar in Sidebar.

--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -1,18 +1,18 @@
 import React from "react";
 import { Container, Header, HeaderIcons } from "./StyledSidebarElements";
+import { Avatar } from "@material-ui/core";
 
 const Sidebar = () => {
     return (
         <div>
             <Container>
                 <Header>
-                    <HeaderIcons>
-                        
-                    </HeaderIcons>
+                    <Avatar src="" alt="avatar" />
+                    <HeaderIcons></HeaderIcons>
                 </Header>
             </Container>
         </div>
-    )
+    );
 };
 
 export default Sidebar;
