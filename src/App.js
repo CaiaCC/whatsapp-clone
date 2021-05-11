5cd620ba14f3a63412d6c68b0a52311759c4a896
Caia Chuang <caiachuang@gmail.com>
2021-05-10 19:06:21 -0700

    Receive messages from the backend.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import Pusher from "pusher-js";
 import "./App.css";
 import Sidebar from "./components/Sidebar";
 import Chat from "./components/Chat";
+import axios from './axios'
 
 const App = () => {
+    const [messages, setMessages] = useState([]);
+
+    useEffect(() => {
+        axios
+            .get('/messages/sync')
+            .then(res => {
+                setMessages(res.data);
+            })
+
+    }, [])
+
     useEffect(() => {
         const pusher = new Pusher("163612a7213d95a362bb", {
             cluster: "mt1",
@@ -15,6 +27,8 @@ const App = () => {
             alert(JSON.stringify(data));
         });
     }, []);
+
+    console.log(messages)
 
     return (
         <div className="app">
