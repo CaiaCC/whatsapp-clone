d4dc98c3a4530c3df1c58bcdeb7d33bf3630caf9
Caia Chuang <caiachuang@gmail.com>
2021-05-11 12:37:44 -0700

    Add Router to App.

--- src/App.js
+++ src/App.js
@@ -11,9 +11,10 @@ const App = () => {
     const [messages, setMessages] = useState([]);
 
     useEffect(() => {
-        axios.get("/messages/sync").then((res) => {
-            setMessages(res.data);
-        });
+        axios.get("/messages/sync")
+            .then((res) => {
+                setMessages(res.data);
+            })
     }, []);
 
     useEffect(() => {
@@ -36,10 +37,10 @@ const App = () => {
         <Main>
             <Router>
                 <Switch>
-                <Route path="/">
-                    <Sidebar />
-                    <Chat messages={messages} />
-                </Route>
+                    <Route path="/">
+                        <Sidebar />
+                        <Chat messages={messages} />
+                    </Route>
                 </Switch>
             </Router>
         </Main>
