b7828703e5517d733214391e4af5ca4ca1f7ece3
Caia Chuang <caiachuang@gmail.com>
2021-05-11 12:36:29 -0700

    Add create room function.

--- src/components/Sidebar/ChatCard.js
+++ src/components/Sidebar/ChatCard.js
@@ -1,42 +1,34 @@
 import React, { useEffect, useState } from 'react';
 import {
-    ChatCardWrap,
+    ChatCard,
     ChatCardInfo,
 } from "./StyledSidebarElements";
 
 import { Avatar } from "@material-ui/core";
 
-const ChatCards = ({ addNewChat }) => {
+const ChatCards = ({ addNewChat, createChat, name }) => {
     const [seed, setSeed] = useState("");
 
     useEffect(() => {
         setSeed(Math.floor(Math.random() * 100));
     }, []);
 
-    const createChat = () => {
-        const roomName = prompt("Please enter name for chat");
-
-        if (roomName) {
-            // data base stuff
-        }
-    }
-
     return !addNewChat ? (
-        <ChatCardWrap>
+        <ChatCard>
             <Avatar
                 src={`https://avatars.dicebear.com/api/human/${seed}.svg`}
                 alt="avatar"
             />
             <ChatCardInfo>
-                <h2>Room name</h2>
+                <h2>{name}</h2>
                 <p>This is the last message</p>
             </ChatCardInfo>
-        </ChatCardWrap>
+        </ChatCard>
     ) : (
-        <ChatCardWrap onClick={createChat}>
+        <ChatCard onClick={createChat}>
             <h2>Add new Chat</h2>
-        </ChatCardWrap>
-    )
+        </ChatCard>
+    );
 };
 
 export default ChatCards;
